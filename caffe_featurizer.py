e01f76fbca0a3068b160889b6b2240138d4a4e07
BenJohnson <BenJohnson@BenJohnsons-MacBook-Pro.local>
2015-11-03 21:14:01 -0500

    ...

--- caffe_featurizer.py
+++ caffe_featurizer.py
@@ -4,21 +4,23 @@ import numpy as np
 import caffe
 
 class CaffeFeaturizer:
-    net = None
+    net         = None
     transformer = None
-    files = []
-    batch_size = None
-
-    def __init__(self, CAFFE_ROOT):
+    files       = []
+    batch_size  = None
+    quiet       = None
+    
+    def __init__(self, CAFFE_ROOT, quiet = False):
         self.caffe_root = CAFFE_ROOT
         caffe.set_mode_cpu()
-        self.net = caffe.Net(self.caffe_root + 'models/bvlc_reference_caffenet/deploy.prototxt', self.caffe_root + 'models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel', caffe.TEST)
+        self.net    = caffe.Net(self.caffe_root + 'models/bvlc_reference_caffenet/deploy.prototxt', self.caffe_root + 'models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel', caffe.TEST)
         transformer = caffe.io.Transformer({'data': self.net.blobs['data'].data.shape})
         transformer.set_transpose('data', (2, 0, 1))
         transformer.set_mean('data', np.load(self.caffe_root + 'python/caffe/imagenet/ilsvrc_2012_mean.npy').mean(1).mean(1))
         transformer.set_raw_scale('data', 255)
         transformer.set_channel_swap('data', (2, 1, 0))
         self.transformer = transformer
+        self.quiet = quiet
 
     def set_batch_size(self, n):
         self.batch_size = n
@@ -35,11 +37,13 @@ class CaffeFeaturizer:
         i = 0
         for f in self.files:
             if i % 10 == 0:
-                print >> sys.stderr,  i
+                if not self.quiet:
+                    print >> sys.stderr,  i
             try:
                 self.net.blobs['data'].data[i] = self.transformer.preprocess('data', caffe.io.load_image(f))
             except:
-                print >> sys.stderr, 'error at %s (%d)' % (f, i)
+                if not self.quiet:
+                    print >> sys.stderr, 'error at %s (%d)' % (f, i)
                 self.errs.append(i)
             i += 1
 
